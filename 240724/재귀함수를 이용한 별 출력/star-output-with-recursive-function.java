49cae1a50439a5c4363e5d0bedfc91bc5ef09acc
SeoHyun Kim <79033073+se0hyun@users.noreply.github.com>
2024-07-24 16:38:31 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 141ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240724/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        printStar(N);
+    }
+
+    public static void printStar(int N){
+        if (N == 0) return ;
+        printStar(N - 1);
+        for(int i = 1; i <= N ; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
