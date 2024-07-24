9a9c84d0934c310e34a86b76e7fc0b3cfe48d879
SeoHyun Kim <79033073+se0hyun@users.noreply.github.com>
2024-07-24 14:05:14 +0900

    Title: 반복 출력하기 2, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

--- 240724/반복 출력하기 2/repeated-output-2.java
+++ 240724/반복 출력하기 2/repeated-output-2.java
@@ -1,16 +1,19 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void printHelloWorld(int N){
             if (N == 0){
-                return ;
+                return;
             }
             printHelloWorld(N - 1);
             System.out.println("HelloWorld");
         }
-        
+
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
 
-        BufferReader br = new BufferReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
 
