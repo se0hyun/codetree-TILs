99a1ad1d1e21b2b57e45ab5bd7992f089760d7f2
SeoHyun Kim <79033073+se0hyun@users.noreply.github.com>
2024-07-26 13:10:32 +0700

    Title: 연속되는 수 3, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240726/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static int MAX_N = 1000;
+    public static int[] arr = new int[MAX_N];
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 1, ans = 0;
+        for (int i = 0; i < N; i++){
+            if (i >= 1 && arr[i] > 0 && arr[i - 1] > 0)
+                cnt++;
+            else if(i >= 1 && arr[i] < 0 && arr[i - 1] < 0)
+                cnt++;
+            else
+                cnt = 1;
+            ans = Math.max(ans, cnt);
+        }
+        System.out.print(ans);
+    }
+}
