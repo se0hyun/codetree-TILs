d0021c47c775a98f8fcceaf0197fa8566ce7b4c5
SeoHyun Kim <79033073+se0hyun@users.noreply.github.com>
2024-08-14 16:09:49 +0900

    Title: 좌우로 움직이는 로봇, Time: 1621ms, Memory: 83MB, Status: Passed - CodeTree

--- 240814/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240814/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -62,15 +62,15 @@ public class Main {
 
         int wasSame = 0, ans = 0; // 마주치면 1, 아니면 0
         for (int i = 1; i < Math.max(idxA, idxB); i++){
-            if (arrA.get(i).equals(arrB.get(i))){   // == 으로 하면 첫번째만 체크함.
-                // System.out.println(i);
-                if (wasSame == 0){
+            if (arrA.get(i).equals(arrB.get(i)) && !arrA.get(i-1).equals(arrB.get(i-1))){   // == 으로 하면 첫번째만 체크함.
+            //     System.out.println(i);
+            //     if (wasSame == 0){
                     ans++;
-                    wasSame = 1;
-                }
+            //         wasSame = 1;
+            //     }
             }
-            else
-                wasSame = 0;
+            // else
+            //     wasSame = 0;
         }
         System.out.print(ans);
     }
