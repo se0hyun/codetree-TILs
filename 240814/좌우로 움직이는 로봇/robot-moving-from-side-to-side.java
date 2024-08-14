7fd5cb51d8af341cde065f714bf4d44719f103a1
SeoHyun Kim <79033073+se0hyun@users.noreply.github.com>
2024-08-14 15:35:00 +0900

    Title: 좌우로 움직이는 로봇, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240814/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int idxA = 1;
+    public static int idxB = 1;
+    public static int getDir(char c){
+        if (c == 'R') return 1;
+        else return -1;
+    }
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> arrA = new ArrayList<>();
+        ArrayList<Integer> arrB = new ArrayList<>();
+        arrA.add(0);
+        arrB.add(0);
+
+        while(n > 0){
+            StringTokenizer stA = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(stA.nextToken());
+            char d = stA.nextToken().charAt(0);
+            for (int i = idxA; i < idxA+t; i++){
+                arrA.add(arrA.get(i-1)+getDir(d));
+            }
+            idxA += t;
+            n--;
+        }
+
+        while(m > 0){
+            StringTokenizer stB = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(stB.nextToken());
+            char d = stB.nextToken().charAt(0);
+            for (int i = idxB; i < idxB+t; i++){
+                arrB.add(arrB.get(i-1)+getDir(d));
+            }
+            idxB += t;
+            m--;
+        }
+
+        // System.out.println(arrA);
+        // System.out.println(arrB);
+
+        if(idxA >idxB){
+            for(int i = idxB; i < idxA; i++){
+                arrB.add(arrB.get(idxB - 1));
+            }
+        }else if(idxA < idxB){
+            for(int i = idxA; i < idxB; i++){
+                arrA.add(arrA.get(idxA - 1));
+            }
+        }
+
+        // System.out.println(arrA);
+        // System.out.println(arrB);
+
+        int wasSame = 0, ans = 0; // 마주치면 1, 아니면 0
+        for (int i = 1; i < Math.max(idxA, idxB); i++){
+            if (arrA.get(i) == arrB.get(i)){
+                if (wasSame == 0){
+                    ans++;
+                    wasSame = 1;
+                    // System.out.println(i);
+                }
+            }else
+                wasSame = 0;
+        }
+        System.out.print(ans);
+    }
+}
+
+// 0, -1, -2, -3, -2, -1, 0, 1, 2, 1, 2, 3
+// 0,  1,  2,  3,  4,  3, 2, 1, 0, 1, 2, 3, 4, 3, 2
+
+// 0, -1, -2, -3, -2, -1, 0, 1, 2, 1, 2, 3, 3, 3, 3, 3
+// 0,  1,  2,  3,  4,  3, 2, 1, 0, 1, 2, 3, 4, 3, 2
+
+// 0, -1, -2, -3, -2, -1, 0, 1, 2, 1, 2, 3, 3, 3, 3
+// 0,  1,  2,  3,  4,  3, 2, 1, 0, 1, 2, 3, 4, 3, 2
